ea6d6a99a289ce00ab941291f957eaa8a91460c6
dhananjayasamantasinghar <52064780+dhananjayasamantasinghar@users.noreply.github.com>
2020-01-28 10:35:40 +0530

    logger

---
+++ Logger_Standalone/common-logger/src/main/java/com/common/logger/utils/ConversationIdGenerator.java
@@ -0,0 +1,27 @@
+package com.common.logger.utils;
+
+import java.util.UUID;
+
+public class ConversationIdGenerator {
+
+	private static final ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+	private ConversationIdGenerator() {
+		super();
+	}
+
+	public static String getConversationId() {
+		if (threadLocal.get() != null) {
+			return threadLocal.get();
+		} else {
+			String uuid = UUID.randomUUID().toString();
+			threadLocal.set(uuid);
+			return uuid;
+		}
+	}
+	
+	
+	public static void clear() {
+	    threadLocal.remove();
+	}
+}
---
+++ Logger_Standalone/test/src/main/java/com/LoggerApplication.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import com.common.logger.utils.ConversationIdGenerator;
+import com.common.logger.utils.LoggingService;
+
+public class LoggerApplication {
+
+	private static LoggingService loggingService = LoggingService.getLogger(LoggerApplication.class);
+
+	public static void main(String[] args) {
+		IntStream.range(0, 10).forEach(e -> {
+			new Thread(() -> {
+				loggingService.logUserJourney(new TestController().get());
+			}).start();
+			try {
+				TimeUnit.SECONDS.sleep(5);
+				System.out.println("===================");
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			ConversationIdGenerator.clear();
+		});
+	}
+
+}
